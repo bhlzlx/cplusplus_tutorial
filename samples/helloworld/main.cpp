ef93ca575e948fb328ebba2790fcbccc27d74a63
bhlzlx <bhlzlx@gmail.com>
2020-03-22 23:39:49 +0800

    更新右值引用工程

--- samples/helloworld/main.cpp
+++ samples/helloworld/main.cpp
@@ -1,6 +1,38 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstdint>
+
+
+class Stu {
+private:
+	uint32_t m_age;
+	uint32_t m_id;
+public:
+	Stu( uint32_t _age, uint32_t _id )
+		: m_age( _age )
+		, m_id( _id )
+	{
+	}
+	Stu(Stu&& _stu) {
+		m_age = _stu.m_age;
+		m_id = _stu.m_id;
+	}
+	uint32_t getAge() {
+		return m_age;
+	}
+	~Stu() {
+		printf("dtor");
+	}
+};
+
+Stu createStudent( uint32_t _age, uint32_t _id ) {
+	return { _age, _id };
+}
+
+int plus(int _x, int _y) {
+	return _x + _y
+}
 
 int main() {
-    printf("hello,world!");
-    return 0;
+	plus(2, 3) = 4;
+	//return stu.getAge();
 }
