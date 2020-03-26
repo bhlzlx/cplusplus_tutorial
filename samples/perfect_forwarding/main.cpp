97696819c306d5658dff783353a765332792c059
bhlzlx <bhlzlx@gmail.com>
2020-03-27 00:50:53 +0800

    update

--- samples/perfect_forwarding/main.cpp
+++ samples/perfect_forwarding/main.cpp
@@ -9,20 +9,20 @@ extern ResourceHandle createExpensiveResource( size_t _size );
 extern void destroyExpensiveResource( ResourceHandle _res );
 extern ResourceHandle copyExpensiveResource( ResourceHandle _res );
 
-class Value {
+class LowLevelValue {
 private:
     ResourceHandle m_resource;
     size_t m_size;
 public:
     // 默认构造函数
-    Value( ResourceHandle _handle, size_t _size ) 
+    LowLevelValue( ResourceHandle _handle, size_t _size ) 
         : m_resource( _handle )
         , m_size( _size )
     {
     }
 
     // 移动构造函数
-    Value( Value&& _v ) {
+    LowLevelValue( LowLevelValue&& _v ) {
         m_resource = _v.m_resource;
         m_size = _v.m_size;
         _v.m_resource = 0;
@@ -30,31 +30,63 @@ public:
     }
 
     // 复制构造函数
-    Value( const Value& _v ) {
+    LowLevelValue( const LowLevelValue& _v ) {
         m_resource = copyExpensiveResource( _v.m_resource );
         m_size = _v.m_size;
     }
 };
 
-template< class T, class ...ARGS > 
-class Factory {
+class HighLevelValue {
+private:
+    LowLevelValue m_res;
 public:
-    T createObject( ARGS && ..._args ) {
-        return T( std::forward<ARGS>(_args)... );
+    HighLevelValue( LowLevelValue&& _value )
+        :m_res(std::move(_value))
+    {
     }
-    T createObject( ARGS ..._args ) {
-        return T( _args ... );
+
+    HighLevelValue( const LowLevelValue& _value )
+        :m_res(_value)
+    {
     }
 };
 
+template< class T, class S > 
+class Factory {
+public:
+    //T createObject( S _arg ) {
+    //    return T(_arg);
+    //}
+    // T createObject( const S &  _arg ) {
+    //     return T(_arg);
+    // }
+    T createObject( S &&  _arg ) {
+        return T(std::forward<S>(_arg));
+    }
+};
+
+// template< class T, class ...ARGS > 
+// class Factory {
+// public:
+//     T createObject( ARGS && ..._args ) {
+//         return T( std::forward<ARGS>(_args)... );
+//     }
+//     T createObject( ARGS ..._args ) {
+//         return T( _args ... );
+//     }
+// };
+
+LowLevelValue createLowLevelValue(){
+    return LowLevelValue ( createExpensiveResource(1024), 1024 );
+}
+
 int main() {
-    Value v1( createExpensiveResource(1024), 1024 );
-    Value v2 = v1;
-    Value v3 = std::move(v1);
+    LowLevelValue v1( createExpensiveResource(1024), 1024 );
+    LowLevelValue v2 = v1;
+    LowLevelValue v3 = std::move(v1);
     printf("%p %p\n", &v2, &v3);
-    Factory<Value, ResourceHandle, size_t> factory;
-    ResourceHandle a = 0; size_t b = 0;
-    factory.createObject( a, b );
+    Factory<HighLevelValue,LowLevelValue> factory;
+    factory.createObject(createLowLevelValue());
     return 0;
 }
 
