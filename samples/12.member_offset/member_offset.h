716e6b1e1bc08de3998caf360ea5450d8600e41f
kusugawa <bhlzlx@gmail.com>
2020-06-08 20:17:30 +0800

    add LRUCache

---
+++ samples/12.member_offset/member_offset.h
@@ -0,0 +1,41 @@
+#include <cstdint>
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+template< class T1, class T2 >
+struct ClazzMemberUnion {
+	union {
+		T1 v1;
+		T2 v2;
+	};
+};
+
+template < class C, class M, class OffsetType = M C::*, std::enable_if_t< sizeof(OffsetType) == 1U, bool > = true, class ReturnType =  uint8_t >
+constexpr ReturnType member_offset_cast__( M C::* && _v ) {
+	ClazzMemberUnion<OffsetType, ReturnType> u = { std::forward<M C::*>(_v) };
+	return u.v2;
+}
+
+template < class C, class M, class OffsetType = M C::*, std::enable_if_t< sizeof(OffsetType) == 2U, bool > = true, class ReturnType =  uint16_t >
+constexpr ReturnType member_offset_cast__( M C::* && _v ) {
+	ClazzMemberUnion<OffsetType, ReturnType> u = { std::forward<M C::*>(_v) };
+	return u.v2;
+}
+
+template < class C, class M, class OffsetType = M C::*, std::enable_if_t< sizeof(OffsetType) == 4U, bool > = true, class ReturnType =  uint32_t >
+constexpr ReturnType member_offset_cast__( M C::* && _v ) {
+	ClazzMemberUnion<OffsetType, ReturnType> u = { std::forward<M C::*>(_v) };
+	return u.v2;
+}
+
+template < class C, class M, class OffsetType = M C::*, std::enable_if_t< sizeof(OffsetType) == 8U, bool > = true, class ReturnType =  uint64_t >
+constexpr ReturnType member_offset_cast__( M C::* && _v ) {
+	ClazzMemberUnion<OffsetType, ReturnType> u = { std::forward<M C::*>(_v) };
+	return u.v2;
+}
+
+template < class T >
+constexpr uint32_t member_offset_cast( T&& _v ) {
+	return static_cast<uint32_t>( member_offset_cast__( std::forward<T>(_v) ) );
+}
