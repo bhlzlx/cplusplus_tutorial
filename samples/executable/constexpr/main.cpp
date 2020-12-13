bd34fee69d2f440d55bb5b556a979464ee4c644d
新 李 <bhlzlx@hotmail.com>
2020-12-13 21:17:11 +0800

    添加 c++14 constexpr 示例

---
+++ samples/executable/constexpr/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+struct Data {
+    int data1;
+    int data2;
+    constexpr Data( int a, int b)
+        : data1(a), data2(b)
+    {}
+    // constexpr 限定不再严格！
+    constexpr int getData1() const {
+        return data1 + data2;
+    }
+    constexpr int getData2() const {
+        return data1 | data2;
+    }
+};
+
+int main() {
+    constexpr Data data(1,2);
+    constexpr auto a = data.getData1(); // 编译期常量，没有函数调用过程和开销
+    auto b = data.getData2(); // 有函数调用过程和开销
+    std::cout<<"data : "<<a<<b<<std::endl;
+    return 0;
+}
