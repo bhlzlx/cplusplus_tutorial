1de569419d525a7cc2665fe90f0da1c1862657ef
kusugawa <bhlzlx@gmail.com>
2021-02-19 15:49:25 +0800

    添加 OneWayBlockedBarrier

--- samples/executable/barrier/main.cpp
+++ samples/executable/barrier/main.cpp
@@ -24,7 +24,7 @@ public:
         ++_counter;
         uint32_t cycle = _cycle;
         uint32_t criticalCountCopy =  _criticalCount;
-        bool rst = _counter.compare_exchange_strong(criticalCountCopy, 0, std::memory_order::relaxed );
+        bool rst = _counter.compare_exchange_strong(criticalCountCopy, 0, std::memory_order::memory_order_relaxed );
         std::unique_lock<std::mutex> lock(_mutex);
         if(!rst) {
             _conditionVariable.wait(lock, [this,&cycle]()->bool{
@@ -50,7 +50,7 @@ public:
     {}
 
     void wait() {
-        std::unique_lock lock(_mutex);
+        std::unique_lock<std::mutex> lock(_mutex);
         if(_counter == 0) {
             /**
              * @brief 为什么wait(lock, predator ) 后边这个Predator是必要的，
@@ -67,7 +67,7 @@ public:
     }
 
     void signal() {
-        std::unique_lock lock(_mutex);
+        std::unique_lock<std::mutex> lock(_mutex);
         bool needNotify = (0==_counter);
         ++_counter;
         if(needNotify) {
@@ -76,13 +76,44 @@ public:
     }
 };
 
+class OneWayBlockedBarrier {
+private:
+    uint32_t                _sigNeed;
+    uint32_t                _sigCounter;
+    std::mutex              _mutex;
+    std::condition_variable _conditionVariable;
+public:
+    OneWayBlockedBarrier( uint32_t sigNeed )
+        : _sigNeed(sigNeed)
+        , _sigCounter(0)
+        , _mutex()
+        , _conditionVariable()
+    {}
+
+    void wait() {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _conditionVariable.wait(lock, [this]()->bool{
+            return _sigNeed == _sigCounter;
+        });
+        _sigCounter = 0; // reset signal counter
+    }
+
+    void signal() {
+        std::unique_lock<std::mutex> lock(_mutex);
+        ++_sigCounter;
+        if(_sigCounter == _sigNeed) {
+            _conditionVariable.notify_one();
+        }
+    }
+};
+
 uint32_t counters[4];
 
-void threadFuncBarrier( uint32_t* counter, Barrier* barrier ) {
+void threadFuncBarrier( uint32_t* counter, OneWayBlockedBarrier* barrier ) {
     for( uint32_t i = 0; i<10000; ++i) {
         (*counter)+=i;
     }
-    barrier->wait();
+    barrier->signal();
 }
 
 void threadFuncSemaphore( uint32_t* counter, Semaphore* Semaphore ) {
@@ -90,7 +121,7 @@ void threadFuncSemaphore( uint32_t* counter, Semaphore* Semaphore ) {
 }
 
 int main() {
-    Barrier barrier(5);
+    OneWayBlockedBarrier barrier(4);
     std::thread threads[4];
     for(uint32_t threadIndex = 0; threadIndex<4; ++threadIndex) {
         threads[threadIndex] = std::thread( threadFuncBarrier, &counters[threadIndex], &barrier);
@@ -100,5 +131,5 @@ int main() {
     for( uint32_t idx = 0; idx<4; ++idx) {
         totalCount += counters[idx];
     }
-    return totalCount;
+    return 0;
 }
