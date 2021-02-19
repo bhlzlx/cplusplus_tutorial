1a50cd70cff78118bee2ef3453f7c7defdab46c2
久寿川 <bhlzlx@gmail.com>
2021-02-19 11:40:56 +0800

    额外处理

--- samples/executable/barrier/main.cpp
+++ samples/executable/barrier/main.cpp
@@ -11,6 +11,7 @@ private:
     std::condition_variable                 _conditionVariable;
     uint32_t                                _criticalCount;
     std::atomic<uint32_t>                   _counter;
+    uint32_t                                _cycle;
 public:
     Barrier( uint32_t criticalCount )
         : _mutex()
@@ -21,11 +22,14 @@ public:
 
     void wait() {
         ++_counter;
+        uint32_t cycle = _cycle;
         uint32_t criticalCountCopy =  _criticalCount;
-        bool rst = _counter.compare_exchange_strong(criticalCountCopy, 0, std::memory_order::memory_order_relaxed );
+        bool rst = _counter.compare_exchange_strong(criticalCountCopy, 0, std::memory_order::relaxed );
         std::unique_lock<std::mutex> lock(_mutex);
         if(!rst) {
-            _conditionVariable.wait(lock);
+            _conditionVariable.wait(lock, [this,&cycle]()->bool{
+                return cycle != _cycle;
+            });
         }
         else {
             _conditionVariable.notify_all();
@@ -33,20 +37,63 @@ public:
     }
 };
 
+class Semaphore {
+private:
+    std::mutex                  _mutex;
+    std::condition_variable     _conditionVariable;
+    std::atomic<uint32_t>       _counter;
+public:
+    Semaphore( uint32_t counter )
+        : _mutex()
+        , _conditionVariable()
+        , _counter(0)
+    {}
+
+    void wait() {
+        std::unique_lock lock(_mutex);
+        if(_counter == 0) {
+            /**
+             * @brief 为什么wait(lock, predator ) 后边这个Predator是必要的，
+             * 因为Linux上应用会出现中断，在wait的时候中断会唤醒线程。这个时候
+             * 并没有notify，所以要加一个predator。
+             */
+            _conditionVariable.wait(lock, [&]()->bool {
+                return _counter != 0;
+            }); // 阻塞
+        }
+        else {
+            --_counter; // 不阻塞，正常通过
+        }
+    }
+
+    void signal() {
+        std::unique_lock lock(_mutex);
+        bool needNotify = (0==_counter);
+        ++_counter;
+        if(needNotify) {
+            _conditionVariable.notify_one();
+        }
+    }
+};
+
 uint32_t counters[4];
 
-void threadFunc( uint32_t* counter, Barrier* barrier ) {
+void threadFuncBarrier( uint32_t* counter, Barrier* barrier ) {
     for( uint32_t i = 0; i<10000; ++i) {
         (*counter)+=i;
     }
     barrier->wait();
 }
 
+void threadFuncSemaphore( uint32_t* counter, Semaphore* Semaphore ) {
+
+}
+
 int main() {
     Barrier barrier(5);
     std::thread threads[4];
     for(uint32_t threadIndex = 0; threadIndex<4; ++threadIndex) {
-        threads[threadIndex] = std::thread( threadFunc, &counters[threadIndex], &barrier);
+        threads[threadIndex] = std::thread( threadFuncBarrier, &counters[threadIndex], &barrier);
     }
     barrier.wait();
     uint32_t totalCount = 0;
