84190c89cb4a636286d2e1c46f59f7032f5a2adc
kusugawa <bhlzlx@gmail.com>
2021-02-21 20:39:00 +0800

    barrier/barrier2/semaphore

--- samples/executable/barrier/main.cpp
+++ samples/executable/barrier/main.cpp
@@ -4,44 +4,93 @@
 #include <condition_variable>
 #include <atomic>
 #include <cstdint>
+#include "../../utility/Timer.h"
 
 class Barrier {
 private:
-    std::mutex                              _mutex;
-    std::condition_variable                 _conditionVariable;
-    uint32_t                                _criticalCount;
-    std::atomic<uint32_t>                   _counter;
-    uint32_t                                _cycle;
+    mutable std::mutex                              _mutex;
+    mutable std::condition_variable                 _conditionVariable;
+    mutable uint32_t                                _waitCount;
+    mutable std::atomic<uint32_t>                   _counter;
+    mutable uint32_t                                _round;
 public:
-    Barrier( uint32_t criticalCount )
+    Barrier( uint32_t waitCount )
         : _mutex()
         , _conditionVariable()
-        , _criticalCount(criticalCount)
+        , _waitCount(waitCount)
         , _counter(0)
     {}
 
-    void wait() {
-        ++_counter;
-        uint32_t cycle = _cycle;
-        uint32_t criticalCountCopy =  _criticalCount;
-        bool rst = _counter.compare_exchange_strong(criticalCountCopy, 0, std::memory_order::memory_order_relaxed );
+    void wait() const {
         std::unique_lock<std::mutex> lock(_mutex);
-        if(!rst) {
-            _conditionVariable.wait(lock, [this,&cycle]()->bool{
-                return cycle != _cycle;
+        auto round = _round;
+        ++_counter;
+        if(_counter != _waitCount) {
+            _conditionVariable.wait(lock, [this,&round]()->bool {
+                return round != _round;
             });
         }
         else {
+            ++_round;
             _conditionVariable.notify_all();
         }
+    }
+
+    void reset() const {
+        std::lock_guard<std::mutex> lock(_mutex);
+        ++_round;
+        _counter = 0;
+        _conditionVariable.notify_all();
+    }
+};
+
+
+class Barrier2 {
+private:
+    mutable std::mutex                              _mutex;
+    mutable std::condition_variable                 _conditionVariable;
+    mutable uint32_t                                _waitCount;
+    mutable std::atomic<uint32_t>                   _counter;
+    mutable uint32_t                                _round;
+public:
+    Barrier2( uint32_t waitCount)
+        : _mutex()
+        , _conditionVariable()
+        , _waitCount(waitCount)
+        , _counter(0)
+    {}
+
+    void sig() const {
+        std::unique_lock<std::mutex> lock(_mutex);
+        ++_counter;
+        if(_counter == _waitCount) {
+            ++_round;
+            _conditionVariable.notify_all();
+        }
+    }
+
+    void wait() const {
+        std::unique_lock<std::mutex> lock(_mutex);
+        if(_counter != _waitCount) {
+            _conditionVariable.wait(lock, [this]()->bool {
+                return _counter == _waitCount;
+            });
+        }
+    }
+
+    void reset() const {
+        std::lock_guard<std::mutex> lock(_mutex);
+        ++_round;
+        _counter = 0;
+        _conditionVariable.notify_all();
     }
 };
 
 class Semaphore {
 private:
-    std::mutex                  _mutex;
-    std::condition_variable     _conditionVariable;
-    std::atomic<uint32_t>       _counter;
+    mutable std::mutex                  _mutex;
+    mutable std::condition_variable     _conditionVariable;
+    mutable std::atomic<uint32_t>       _counter;
 public:
     Semaphore( uint32_t counter )
         : _mutex()
@@ -49,15 +98,15 @@ public:
         , _counter(0)
     {}
 
-    void wait() {
+    void wait() const {
         std::unique_lock<std::mutex> lock(_mutex);
+        /**
+        * @brief 为什么wait(lock, predator ) 后边这个Predator是必要的，
+        * 因为Linux上应用会出现中断，在wait的时候中断会唤醒线程。这个时候
+        * 并没有notify，所以要加一个predator。
+        */
         if(_counter == 0) {
-            /**
-             * @brief 为什么wait(lock, predator ) 后边这个Predator是必要的，
-             * 因为Linux上应用会出现中断，在wait的时候中断会唤醒线程。这个时候
-             * 并没有notify，所以要加一个predator。
-             */
-            _conditionVariable.wait(lock, [&]()->bool {
+           _conditionVariable.wait(lock, [&]()->bool {
                 return _counter != 0;
             }); // 阻塞
         }
@@ -66,70 +115,39 @@ public:
         }
     }
 
-    void signal() {
+    void sig() const {
         std::unique_lock<std::mutex> lock(_mutex);
-        bool needNotify = (0==_counter);
         ++_counter;
-        if(needNotify) {
-            _conditionVariable.notify_one();
-        }
-    }
-};
-
-class OneWayBlockedBarrier {
-private:
-    uint32_t                _sigNeed;
-    uint32_t                _sigCounter;
-    std::mutex              _mutex;
-    std::condition_variable _conditionVariable;
-public:
-    OneWayBlockedBarrier( uint32_t sigNeed )
-        : _sigNeed(sigNeed)
-        , _sigCounter(0)
-        , _mutex()
-        , _conditionVariable()
-    {}
-
-    void wait() {
-        std::unique_lock<std::mutex> lock(_mutex);
-        _conditionVariable.wait(lock, [this]()->bool{
-            return _sigNeed == _sigCounter;
-        });
-        _sigCounter = 0; // reset signal counter
-    }
-
-    void signal() {
-        std::unique_lock<std::mutex> lock(_mutex);
-        ++_sigCounter;
-        if(_sigCounter == _sigNeed) {
-            _conditionVariable.notify_one();
-        }
+        _conditionVariable.notify_one();
     }
 };
 
 uint32_t counters[4];
 
-void threadFuncBarrier( uint32_t* counter, OneWayBlockedBarrier* barrier ) {
-    for( uint32_t i = 0; i<10000; ++i) {
-        (*counter)+=i;
-    }
-    barrier->signal();
-}
-
-void threadFuncSemaphore( uint32_t* counter, Semaphore* Semaphore ) {
-
+void threadFuncBarrier( uint32_t* counter, Barrier2* barrier ) {
+    std::this_thread::sleep_for(std::chrono::seconds(1)); // 假设算法耗时1秒
+    barrier->sig();
 }
 
 int main() {
-    OneWayBlockedBarrier barrier(4);
+    Barrier2 barrier(4);
     std::thread threads[4];
+
+    // 创建4个任务，并发执行，总耗时也就一秒多一些。
     for(uint32_t threadIndex = 0; threadIndex<4; ++threadIndex) {
         threads[threadIndex] = std::thread( threadFuncBarrier, &counters[threadIndex], &barrier);
     }
+    cpp::Timer timer;
     barrier.wait();
     uint32_t totalCount = 0;
     for( uint32_t idx = 0; idx<4; ++idx) {
         totalCount += counters[idx];
+    }
+    uint32_t multiThreadDua = timer.duration();
+    printf("%u\n", multiThreadDua);
+
+    for( auto& thread: threads) {
+        thread.join();
     }
     return 0;
 }
